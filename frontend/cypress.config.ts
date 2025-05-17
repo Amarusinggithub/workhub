ea63fab07271c2f89789e0b51d1004ccc8c704bc
Amarusinggithub <loveamar285@gmail.com>
2025-05-17 08:15:59 -0500

    added cypress

---
+++ frontend/cypress.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(
+        on: Cypress.PluginEvents,
+        config: Cypress.PluginConfigOptions
+    ) {
+      return config;
+      // implement node event listeners here
+    },
+  },
+});
